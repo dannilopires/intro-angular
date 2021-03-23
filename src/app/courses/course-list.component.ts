f30021ca3cdc25e869c7c81a9b7d0cb49c69c96c
dannilo <dannilo-pires@hotmail.com>
2021-03-22 21:37:45 -0300

    add service - injeçao de dependencia - retrieveAll()

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -9,29 +10,12 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit{
         courses: Course[] = [];
 
+        constructor(private courseService: CourseService){}
+
         // Método ngOnInit diz respeito ao ciclo de vida do componente.
         ngOnInit(): void{
-            this.courses = [
-                {
-                    id: 1,
-                    name: 'Angular Forms',
-                    imageUrl: '/assets/images/forms.png',
-                    price: 99.99,
-                    code: 'XPS-8796',
-                    duration: 120,
-                    rating: 4.5,
-                    releaseDate: '4 de Abril'
-                },
-                {
-                    id: 2,
-                    name: 'Angular HTTP',
-                    imageUrl: '/assets/images/http.png',
-                    price: 49.99,
-                    code: 'XPS-4057',
-                    duration: 50,
-                    rating: 4,
-                    releaseDate: '20 de Abril'
-                }
-            ];
+            this.courses = this.courseService.retrieveAll();
+
         }
+
 }
